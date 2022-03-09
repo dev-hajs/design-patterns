56ffa781739d85d9997236bd06bd84a2c8bf1975
하지수 <dev.hajs@gmail.com>
2022-03-10 01:03:48 +0900

    prototype pattern 2 -. how to use prototype pattern

---
+++ src/main/java/com/study/designpatterns/_01_creational_patterns/_05_prototype/_02_after/GithubRepository.java
@@ -0,0 +1,13 @@
+package com.study.designpatterns._01_creational_patterns._05_prototype._02_after;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GithubRepository {
+
+    private String user;
+
+    private String name;
+}
