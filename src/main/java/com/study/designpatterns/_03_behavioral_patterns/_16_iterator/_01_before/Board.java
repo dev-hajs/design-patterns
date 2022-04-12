1a2b40050034c17d5c5e0222a9fd66126eb7e3ac
하지수 <dev.hajs@gmail.com>
2022-04-12 22:17:17 +0900

    iterator pattern 1 - introduce pattern

---
+++ src/main/java/com/study/designpatterns/_03_behavioral_patterns/_16_iterator/_01_before/Board.java
@@ -0,0 +1,17 @@
+package com.study.designpatterns._03_behavioral_patterns._16_iterator._01_before;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    private List<Post> posts = new ArrayList<>();
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void addPost(String content) {
+        this.posts.add(new Post(content));
+    }
+}
