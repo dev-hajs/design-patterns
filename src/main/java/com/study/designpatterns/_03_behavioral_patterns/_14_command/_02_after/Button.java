ae6ad9ab521dc368e276a666f7f7116a8f23810f
하지수 <dev.hajs@gmail.com>
2022-04-12 21:16:43 +0900

    command pattern 3 - pros and cons

--- src/main/java/com/study/designpatterns/_03_behavioral_patterns/_14_command/_02_after/Button.java
+++ src/main/java/com/study/designpatterns/_03_behavioral_patterns/_14_command/_02_after/Button.java
@@ -1,23 +1,31 @@
 package com.study.designpatterns._03_behavioral_patterns._14_command._02_after;
 
 import com.study.designpatterns._03_behavioral_patterns._14_command._01_before.Game;
+import com.study.designpatterns._03_behavioral_patterns._14_command._01_before.Light;
+
+import java.util.Stack;
 
 public class Button {
 
-    private Command command;
+    private Stack<Command> commands = new Stack<>();
 
-    public Button(Command command) {
-        this.command = command;
+    public void press(Command command) {
+        command.execute();
+        commands.push(command);
     }
 
-    public void press() {
-        command.execute();
+    public void undo() {
+        if (!commands.isEmpty()) {
+            Command pop = commands.pop();
+            pop.undo();
+        }
     }
 
     public static void main(String[] args) {
-        Button button = new Button(new GameStartCommand(new Game()));
-
-        button.press();
-        button.press();
+        Button button = new Button();
+        button.press(new GameStartCommand(new Game()));
+        button.press(new LightOnCommand( new Light()));
+        button.undo();
+        button.undo();
     }
 }
