1452f8cd1de99733c45bbe80705db70c2892cc80
하지수 <dev.hajs@gmail.com>
2022-05-19 23:58:37 +0900

    apply lombok annotaion in chapter18

--- src/main/java/com/study/designpatterns/_03_behavioral_patterns/_18_memento/_02_after/Game.java
+++ src/main/java/com/study/designpatterns/_03_behavioral_patterns/_18_memento/_02_after/Game.java
@@ -1,25 +1,14 @@
 package com.study.designpatterns._03_behavioral_patterns._18_memento._02_after;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Game {
 
     private int redTeamScore;
     private int blueTeamScore;
-
-    public int getRedTeamScore() {
-        return redTeamScore;
-    }
-
-    public void setRedTeamScore(int redTeamScore) {
-        this.redTeamScore = redTeamScore;
-    }
-
-    public int getBlueTeamScore() {
-        return blueTeamScore;
-    }
-
-    public void setBlueTeamScore(int blueTeamScore) {
-        this.blueTeamScore = blueTeamScore;
-    }
 
     public GameSave save() {
         return new GameSave(this.blueTeamScore, this.redTeamScore);
